1de569a2a5414383b06c5cf6e4977e4824b055fd
IliaLataria01 <latariailia6@gmail.com>
2024-02-11 21:19:55 +0400

    First paragraph: Lambda expressions - API functional interfaces (Supplier,Consumer and BiConsumer)

--- src/main/java/org/example/lambda_expressions/APIFunctionalInterfaces.java
+++ src/main/java/org/example/lambda_expressions/APIFunctionalInterfaces.java
@@ -1,7 +1,10 @@
 package org.example.lambda_expressions;
 
-import java.util.function.BiPredicate;
-import java.util.function.Predicate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.*;
 
 public class APIFunctionalInterfaces {
 
@@ -15,9 +18,43 @@ public class APIFunctionalInterfaces {
         System.out.println("Using BiPredicate " + biPredicate.test("Hello Bro",29));
     }
 
+    public void supplier() {
+        Supplier<StringBuilder> supplier_first = StringBuilder::new;
+        System.out.println("First case of using Supplier: " + supplier_first.get().append("February 11") );
+
+        Supplier<LocalTime> supplier_second = LocalTime::now;
+        System.out.println("Second case of using Supplier: " + supplier_second.get());
+    }
+
+    public void consumer() {
+        Consumer<String> consumer = s -> System.out.println(s);
+        consumer.accept("First case of using consumer");
+
+        List<String > names = new ArrayList<>();
+        names.add("X");
+        names.add("Y");
+        names.add("Z");
+        names.forEach(consumer);
+    }
+
+    public void biConsumer() {
+        HashMap<String,String> mapCapabilities = new HashMap<String,String>();
+
+        BiConsumer<String,String> biConsumer = (key,value) -> mapCapabilities.put(key,value);
+
+        biConsumer.accept("Basketball","Lebron James");
+        biConsumer.accept("Football","Cristiano Ronaldo");
+
+        System.out.println(mapCapabilities);
+
+    }
+
     public static void main(String[] args) {
         APIFunctionalInterfaces apiFunctionalInterfaces = new APIFunctionalInterfaces();
         apiFunctionalInterfaces.predicate();
         apiFunctionalInterfaces.biPredicate();
+        apiFunctionalInterfaces.supplier();
+        apiFunctionalInterfaces.consumer();
+        apiFunctionalInterfaces.biConsumer();
     }
 }
