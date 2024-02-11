70345f6798ffd6e2be66e9762eb372f37bd77d67
IliaLataria01 <latariailia6@gmail.com>
2024-02-11 21:05:12 +0400

    First paragraph: Lambda expressions - added custome code for Predicate

--- src/main/java/org/example/lambda_expressions/TestPredicate.java
+++ src/main/java/org/example/lambda_expressions/TestPredicate.java
@@ -17,5 +17,16 @@ public class TestPredicate {
         System.out.println("First Evaluation using Predicate (functional interface): " + predicate.test(10));
         System.out.println("Second Evaluation using Predicate (functional interface): " + predicate.test(-10));
 
+
+        int x = 4;
+        System.out.println("Is " + x + " even ? " + check(x,n -> n % 2 == 0));
+        x = 7;
+        System.out.println("Is " + x + " even ? " + check(x,n -> n % 2 == 0));
+
     }
+
+    public static <T> boolean check(T t,Predicate<T> lambda) {
+        return lambda.test(t);
+    }
+
 }
