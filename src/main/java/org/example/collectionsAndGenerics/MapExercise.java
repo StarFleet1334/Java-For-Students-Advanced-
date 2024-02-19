14db02b61ebeeb3f1a9bc55140025b7b882f02d1
IliaLataria01 <latariailia6@gmail.com>
2024-02-19 22:16:08 +0400

    third paragraph: Collections & Genereics - Map,Queue,Dequeue,ArrayQueue, LinkedList and PriorityQueue

--- src/main/java/org/example/collectionsAndGenerics/MapExercise.java
+++ src/main/java/org/example/collectionsAndGenerics/MapExercise.java
@@ -1,12 +1,21 @@
 package org.example.collectionsAndGenerics;
 
 
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 /**
  *
  *
+ *  HashMap
+ *      -> Unsorted,UnOrdered Map
+ *      -> It uses Hashcode of the object when inserting
+ *      -> It allows null keys and multiple null values
  *
- *
- *
+ *  HashTable
+ *      -> Is similar to hashMap but is slower and
+ *      -> It does not allow nulls and also is thread-safe
  *
  *
  *
@@ -14,4 +23,45 @@ package org.example.collectionsAndGenerics;
 
 
 public class MapExercise {
+
+    public static void main(String[] args) {
+
+        Map<String,Integer> map = new TreeMap<>(); // Sorted by Keys
+        map.put("John",10);
+        map.put("Mary",21);
+        map.put("Chris",33);
+        System.out.println(map.containsKey("John")); // true
+        System.out.println(map.containsValue(18)); // false
+        System.out.println(map.containsValue(21)); // true
+        System.out.println(map.isEmpty()); // false
+        System.out.println(map.get("Chris")); // 33
+        for (String name : map.keySet()) {
+            System.out.println(name);
+        }
+        for (Integer age : map.values()) {
+            System.out.println(age);
+        }
+
+        System.out.println(map.size()); // 3
+        map.clear();
+        System.out.println(map.size()); // 0
+        map.put("John",10);
+        map.put("Mary",21);
+        map.put("Chris",33);
+
+        map.forEach((x,y) -> System.out.println(x + " : " + y));
+        for (Map.Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
+            System.out.println(stringIntegerEntry.getKey() + " " + stringIntegerEntry.getValue());
+        }
+
+
+        Set keys = map.keySet();
+        map.put("Mike",null); // [Chris=33,John=18,Mary=21,Mike=null]
+        map.putIfAbsent("Chris",99); // [Chris=33,John=18,Mary=21,Mike=null]
+        map.putIfAbsent("Mike",55); // [Chris=3,John=18,Mary=21,Mike=55]
+        map.putIfAbsent("Luke",31); // [Chris=3,John=18,Luke=31,Mary=21,Mike=55]
+        System.out.println(keys);
+
+    }
+
 }
---
+++ src/main/java/org/example/collectionsAndGenerics/QueueAndDequeueExercise.java
@@ -0,0 +1,117 @@
+package org.example.collectionsAndGenerics;
+
+import org.example.streams.exam.solution.Book;
+
+import java.util.*;
+
+/***
+ *
+ * Queue - a collection that specifies the order in which elements are to be processed.
+ *
+ * Note:
+ *  -> LinkedList implements Queue
+ *
+ *
+ *  PriorityQueue
+ *      -> orders the elements relative to each other such that
+ *          "priority-in, priority-out"
+ *      -> the elements are either ordered by natural order or by custom comparator
+ *      -> elements that are sorted first will be accessed first
+ *
+ *  Dequeue
+ *      -> access from both ends permitted
+ *      -> can be used as both FIFO(queue) and LIFO (Stack)
+ *
+ *  ArrayDequeue
+ *      -> expandable array implementation of the Dequeue interface
+ *
+ *
+ *
+ */
+
+
+public class QueueAndDequeueExercise {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void linkedListQueue() {
+        // FIFO queue -> First in and First out
+        // add() inserts into queue but will throw exception if no space exists that happens when
+        // capacity is restricted. offer() inserts into queue and returns false in the same scenario
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.offer(2);
+        queue.add(3);
+        queue.offer(4);
+
+        // element() retrieves but does not remove head of the queue, but in case if queue is empty
+        // it throws exception and peek() same thing but in same scenario it returns null if queue
+        // is empty
+        System.out.println("Head of the queue: " + queue.element());
+        System.out.println("Head of the queue: " + queue.peek());
+        System.out.println(queue);
+
+        // remove() retrieves and removes head of the queue and throws exception if queue is empty
+        // poll() retrieves and removes the head of this queue and returns null if it is empty
+        System.out.println("Removed head: " + queue.remove() + ", remaining queue: " + queue);
+        System.out.println("Removed head: " + queue.poll() + ", remaining queue: " + queue);
+    }
+
+    public static void arrayDequeue() {
+        // Resizeable array implementation of the dequeue interface (no capacity restrictions)
+        Deque<Integer> numbers = new ArrayDeque<>();
+        numbers.add(1); // [1]
+        numbers.addFirst(2); // [2,1]
+        numbers.offerFirst(3); // [3,2,1]
+        System.out.println("Head : " + numbers.getFirst() + ", Head: " + numbers.peek());
+
+        numbers.addLast(4); // [3,2,1,4]
+        numbers.offerFirst(5); // [3,2,1,4,5]
+
+        numbers.removeFirst(); // [2,1,4,5]
+        numbers.pollFirst(); // [1,4,5]
+        numbers.removeLast(); // [1,4]
+        numbers.pollLast(); // [1]
+        System.out.println(numbers);
+    }
+
+    public static void priorityQueueNaturalOrdering() {
+        Queue<String> names = new PriorityQueue<>();
+        names.add("V");
+        names.add("P");
+        names.add("A");
+        Iterator<String> iterator = names.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(names.poll() + " ");
+        }
+
+        Queue<Integer> numbers = new PriorityQueue<>();
+        numbers.add(11);
+        numbers.add(5);
+        numbers.add(2);
+        Iterator<Integer> iterator1 = numbers.iterator();
+        while (iterator1.hasNext()) {
+            System.out.println(numbers.poll() + " ");
+        }
+    }
+
+    public static void priorityQueueDifferentOrdering() {
+        // Ordering specified by a comparator at construction time
+        // Ordering by the title of the book
+        Comparator<Book> comparator = Comparator.comparing(book -> book.getTitle());
+        Queue<Book> booksByTitle = new PriorityQueue<>(comparator);
+        booksByTitle.add(new Book("Java",55.0));
+        booksByTitle.add(new Book("Python",23.0));
+        booksByTitle.add(new Book("C++",99.0));
+
+        System.out.println("Ordering by title: ");
+        Iterator<Book> iterator = booksByTitle.iterator();
+        while (iterator.hasNext()) {
+            Book book = booksByTitle.poll();
+            System.out.println(book);
+        }
+    }
+}
