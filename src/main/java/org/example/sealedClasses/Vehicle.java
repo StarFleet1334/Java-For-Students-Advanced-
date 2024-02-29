0a19245ed35c805e33ce640ce48a51eae16ba2cf
IliaLataria01 <latariailia6@gmail.com>
2024-03-01 00:23:11 +0400

    Java 17: Selead classes - Theory + code

---
+++ src/main/java/org/example/sealedClasses/Vehicle.java
@@ -0,0 +1,21 @@
+package org.example.sealedClasses;
+
+
+sealed interface Driveable permits Vehicle {}
+
+public sealed class Vehicle implements Driveable permits Car{
+
+}
+
+sealed class Car extends Vehicle permits Ford,Volvo {
+
+}
+
+final class Ford extends Car {}
+final class Volvo extends Car {}
+
+
+
+// if you uncomment this it would give error:
+// 'Table' is not allowed in the sealed hierarchy
+//class Table implements Driveable{}
